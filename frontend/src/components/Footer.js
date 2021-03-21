6d8eef2ac0ddedf1e0b83e5dace7de01b9a4a451
afshoang <kaitokid19xx@gmail.com>
2021-03-22 05:51:37 +0700

    Footer

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+
+const Footer = () => {
+  return (
+    <footer className='bg-primary' variant='dark'>
+      <Container>
+        <Row className='py-3'>
+          <Col>
+            <h5>CÔNG TY CP ĐẦU TƯ PHÁT TRIỂN QUỐC TẾ MIXMILK</h5>
+            <p>Số 391 Nguyễn Xiển, Kim Giang, Thanh Xuân, Hà Nội</p>
+            <p>
+              <i class='fas fa-phone-alt'></i> 1900.9999
+            </p>
+            <p>
+              <i class='far fa-envelope'></i> mixmilk@gmail.com
+            </p>
+          </Col>
+          {/* <Col className='text-center py-3'>Copyright &copy; HoangPham</Col> */}
+        </Row>
+      </Container>
+    </footer>
+  );
+};
+
+export default Footer;
