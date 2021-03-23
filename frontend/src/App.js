1c0270128826e32e3565f3ac568c9805d7124bd8
afshoang <kaitokid19xx@gmail.com>
2021-03-23 10:08:47 +0700

    add route product screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Header />
       <main>
         <Route path='/' component={HomeScreen} exact />
+        <Route path='/product/:id' component={ProductScreen} />
       </main>
       <Footer />
     </Router>
