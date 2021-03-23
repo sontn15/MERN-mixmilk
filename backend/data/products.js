87653b39a9f06dd007e73d2ee1c314347bc55653
afshoang <kaitokid19xx@gmail.com>
2021-03-23 16:28:43 +0700

    set up backend server

---
+++ backend/data/products.js
@@ -0,0 +1,116 @@
+const products = [
+  {
+    _id: '1',
+    productCode: 'Pro05',
+    name: 'CURCUMIX (DẠNG HỘP CÓ GÓI NHỎ ĐỂ PHA)',
+    image: '/images/curcumix.jpg',
+    description: {
+      thanhPhan:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, error? Eveniet veritatis necessitatibus excepturi amet.',
+      congDung:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, error? Eveniet veritatis necessitatibus excepturi amet.',
+    },
+    brand: 'Vesta',
+    category: 'Thực phẩm bảo vệ sức khỏe',
+    price: 500000,
+    countInStock: 50,
+  },
+  {
+    _id: '2',
+    productCode: 'GM2',
+    name: 'GOLDEN MILK (TÚI VỈ)',
+    image: '/images/goldenmilk.jpg',
+    description: {
+      thanhPhan:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, error? Eveniet veritatis necessitatibus excepturi amet.',
+      congDung:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, error? Eveniet veritatis necessitatibus excepturi amet.',
+    },
+    brand: 'Vesta',
+    category: 'Thực phẩm bảo vệ sức khỏe',
+    price: 150000,
+    countInStock: 50,
+  },
+  {
+    _id: '3',
+    productCode: '10000',
+    name: "S'COFFEE CAFE ĐÔNG TRÙNG HẠ THẢO",
+    image: '/images/scoffee.jpg',
+    description: {
+      thanhPhan:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, error? Eveniet veritatis necessitatibus excepturi amet.',
+      congDung:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, error? Eveniet veritatis necessitatibus excepturi amet.',
+    },
+    brand: 'Vesta',
+    category: 'Thực phẩm bảo vệ sức khỏe',
+    price: 500000,
+    countInStock: 50,
+  },
+  {
+    _id: '4',
+    productCode: 'Pro01',
+    name: 'VIÊN SỮA MIXMILK HƯƠNG CHOCOLATE',
+    image: '/images/mixmilk1.jpg',
+    description: {
+      thanhPhan:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, error? Eveniet veritatis necessitatibus excepturi amet.',
+      congDung:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, error? Eveniet veritatis necessitatibus excepturi amet.',
+    },
+    brand: 'Vesta',
+    category: 'Thực phẩm bảo vệ sức khỏe',
+    price: 250000,
+    countInStock: 50,
+  },
+  {
+    _id: '5',
+    productCode: 'Pro02',
+    name: 'VIÊN SỮA MIXMILK HƯƠNG DÂU',
+    image: '/images/mixmilk3.jpg',
+    description: {
+      thanhPhan:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, error? Eveniet veritatis necessitatibus excepturi amet.',
+      congDung:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, error? Eveniet veritatis necessitatibus excepturi amet.',
+    },
+    brand: 'Vesta',
+    category: 'Thực phẩm bảo vệ sức khỏe',
+    price: 250000,
+    countInStock: 50,
+  },
+  {
+    _id: '1',
+    productCode: 'Pro03',
+    name: 'VIÊN SỮA MIXMILK HƯƠNG VANI',
+    image: '/images/mixmilk2.jpg',
+    description: {
+      thanhPhan:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, error? Eveniet veritatis necessitatibus excepturi amet.',
+      congDung:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, error? Eveniet veritatis necessitatibus excepturi amet.',
+    },
+    brand: 'Vesta',
+    category: 'Thực phẩm bảo vệ sức khỏe',
+    price: 250000,
+    countInStock: 50,
+  },
+  {
+    _id: '6',
+    productCode: 'Pro04',
+    name: 'VITA CARNOISIN (VIÊN NGẬM HOẶC NHAI)',
+    image: '/images/vita-carnoisin.jpg',
+    description: {
+      thanhPhan:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, error? Eveniet veritatis necessitatibus excepturi amet.',
+      congDung:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, error? Eveniet veritatis necessitatibus excepturi amet.',
+    },
+    brand: 'Vesta',
+    category: 'Thực phẩm bảo vệ sức khỏe',
+    price: 500000,
+    countInStock: 50,
+  },
+];
+
+module.exports = products;
