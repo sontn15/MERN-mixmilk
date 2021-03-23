19d28252a0355d32568b4436211672e63da6677b
afshoang <kaitokid19xx@gmail.com>
2021-03-23 08:37:35 +0700

    about creen

---
+++ frontend/src/screens/AboutScreen.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+const AboutScreen = () => {
+  return (
+    <Container>
+      <h5>Thư ngỏ</h5>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, magnam.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti sit
+        similique expedita inventore commodi voluptatem facilis, distinctio
+        iusto voluptates tempora!
+      </p>
+      <img src='/images/mixmilk1.jpg' />
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti sit
+        similique expedita inventore commodi voluptatem facilis, distinctio
+        iusto voluptates tempora!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti sit
+        similique expedita inventore commodi voluptatem facilis, distinctio
+        iusto voluptates tempora!
+      </p>
+      <img src='/images/mixmilk1.jpg' />
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti sit
+        similique expedita inventore commodi voluptatem facilis, distinctio
+        iusto voluptates tempora!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti sit
+        similique expedita inventore commodi voluptatem facilis, distinctio
+        iusto voluptates tempora!
+      </p>
+    </Container>
+  );
+};
+
+export default AboutScreen;
