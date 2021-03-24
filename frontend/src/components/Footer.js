36c75216272c36bfc28b6eb51573170b635162aa
afshoang <kaitokid19xx@gmail.com>
2021-03-25 06:57:20 +0700

    class => className

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -10,10 +10,10 @@ const Footer = () => {
             <h5>CÔNG TY CP ĐẦU TƯ PHÁT TRIỂN QUỐC TẾ MIXMILK</h5>
             <p>Số 391 Nguyễn Xiển, Kim Giang, Thanh Xuân, Hà Nội</p>
             <p>
-              <i class='fas fa-phone-alt'></i> 1900.9999
+              <i className='fas fa-phone-alt'></i> 1900.9999
             </p>
             <p>
-              <i class='far fa-envelope'></i> mixmilk@gmail.com
+              <i className='far fa-envelope'></i> mixmilk@gmail.com
             </p>
           </Col>
           {/* <Col className='text-center py-3'>Copyright &copy; HoangPham</Col> */}
