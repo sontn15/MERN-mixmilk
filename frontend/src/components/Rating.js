cd5b6d499e08051614b406df954a65bec489371b
afshoang <kaitokid19xx@gmail.com>
2021-03-26 08:43:46 +0700

    miss typing

--- frontend/src/components/Rating.js
+++ frontend/src/components/Rating.js
@@ -76,7 +76,7 @@ Rating.defaultProps = {
 Rating.propTypes = {
   value: PropTypes.number.isRequired,
   text: PropTypes.string.isRequired,
-  value: PropTypes.string,
+  color: PropTypes.string,
 };
 
 export default Rating;
