6c775090f14de3e81f0763d99e26294faf6e8fa6
afshoang <kaitokid19xx@gmail.com>
2021-03-29 07:35:51 +0700

    update profile screen

---
+++ frontend/src/screens/ProfileScreen.js
@@ -0,0 +1,111 @@
+import React, { useState, useEffect } from 'react';
+import { Container, Form, Button, Row, Col } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
+import Message from '../components/Message';
+import Loader from '../components/Loader';
+import { getUserDetails, updateUserProfile } from '../actions/userActions';
+
+const ProfileScreen = ({ location, history }) => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [message, setMessage] = useState(null);
+
+  const dispatch = useDispatch();
+
+  const userDetails = useSelector((state) => state.userDetails);
+  const { loading, error, user } = userDetails;
+
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
+
+  const userUpdateProfile = useSelector((state) => state.userUpdateProfile);
+  const { success } = userUpdateProfile;
+
+  useEffect(() => {
+    if (!userInfo) {
+      history.push('/login');
+    } else {
+      if (!user.name) {
+        dispatch(getUserDetails('profile'));
+      } else {
+        setName(user.name);
+        setEmail(user.email);
+      }
+    }
+  }, [dispatch, history, userInfo, user]);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    if (password !== confirmPassword) {
+      setMessage('Mật khẩu không khớp nhau!');
+    } else {
+      dispatch(updateUserProfile({ id: user._id, name, email, password }));
+    }
+  };
+
+  return (
+    <Container>
+      <Row>
+        <Col md={3}>
+          <h2>User Profile</h2>
+          {message && <Message variant='danger'>{message}</Message>}
+          {error && <Message variant='danger'>{error}</Message>}
+          {success && <Message variant='success'>Profile Updated</Message>}
+          {loading && <Loader />}
+          <Form onSubmit={submitHandler}>
+            <Form.Group controlId='name'>
+              <Form.Label>Tên</Form.Label>
+              <Form.Control
+                type='name'
+                placeholder='Nhập tên'
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+
+            <Form.Group controlId='email'>
+              <Form.Label>Email</Form.Label>
+              <Form.Control
+                type='email'
+                placeholder='Nhập email'
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+
+            <Form.Group controlId='password'>
+              <Form.Label>Mật khẩu</Form.Label>
+              <Form.Control
+                type='password'
+                placeholder='Nhập mật khẩu'
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+
+            <Form.Group controlId='confirmpassword'>
+              <Form.Label>Xác nhận mật khẩu</Form.Label>
+              <Form.Control
+                type='password'
+                placeholder='Xác nhận mật khẩu'
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+
+            <Button type='submit' variant='primary'>
+              Update
+            </Button>
+          </Form>
+        </Col>
+        <Col md={9}>
+          <h2>My orders</h2>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default ProfileScreen;
