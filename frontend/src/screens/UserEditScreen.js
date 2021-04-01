d854dd954ef192d5582d8654e69af6d992b5af30
afshoang <kaitokid19xx@gmail.com>
2021-04-01 07:54:48 +0700

    user Edit screen

---
+++ frontend/src/screens/UserEditScreen.js
@@ -0,0 +1,104 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { Container, Form, Button } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
+import Message from '../components/Message';
+import Loader from '../components/Loader';
+import FormContainer from '../components/FormContainer';
+import { getUserDetails, updateUser } from '../actions/userActions';
+import { USER_UPDATE_RESET } from '../constants/userConstants';
+
+const UserEditScreen = ({ match, history }) => {
+  const userId = match.params.id;
+
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [isAdmin, setIsAdmin] = useState(false);
+
+  const dispatch = useDispatch();
+
+  const userDetails = useSelector((state) => state.userDetails);
+  const { loading, error, user } = userDetails;
+
+  const userUpdate = useSelector((state) => state.userUpdate);
+  const {
+    loading: loadingUpdate,
+    error: errorUpdate,
+    success: successUpdate,
+  } = userUpdate;
+
+  useEffect(() => {
+    if (successUpdate) {
+      dispatch({ type: USER_UPDATE_RESET });
+      history.push('/admin/userlist');
+    } else {
+      if (!user.name || user._id !== userId) {
+        dispatch(getUserDetails(userId));
+      } else {
+        setName(user.name);
+        setEmail(user.email);
+        setIsAdmin(user.isAdmin);
+      }
+    }
+  }, [dispatch, history, user, userId, successUpdate]);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    dispatch(updateUser({ _id: userId, name, email, isAdmin }));
+  };
+
+  return (
+    <Container>
+      <Link to='/admin/userlist' className='btn btn-light my-3'>
+        Trở về
+      </Link>
+      <FormContainer>
+        <h1>Cập nhật thông tin khách hàng</h1>
+        {loadingUpdate && <Loader />}
+        {errorUpdate && <Message variant='danger'>{errorUpdate}</Message>}
+        {loading ? (
+          <Loader />
+        ) : error ? (
+          <Message variant='danger'>{error}</Message>
+        ) : (
+          <Form onSubmit={submitHandler}>
+            <Form.Group controlId='name'>
+              <Form.Label>Tên</Form.Label>
+              <Form.Control
+                type='name'
+                placeholder='Nhập tên'
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+
+            <Form.Group controlId='email'>
+              <Form.Label>Email</Form.Label>
+              <Form.Control
+                type='email'
+                placeholder='Nhập email'
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+
+            <Form.Group controlId='isAdmin'>
+              <Form.Check
+                type='checkbox'
+                label='Quyền Admin'
+                checked={isAdmin}
+                onChange={(e) => setIsAdmin(e.target.checked)}
+              ></Form.Check>
+            </Form.Group>
+
+            <Button type='submit' variant='primary'>
+              Cập nhật
+            </Button>
+          </Form>
+        )}
+      </FormContainer>
+    </Container>
+  );
+};
+
+export default UserEditScreen;
