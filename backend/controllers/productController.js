ec5939195babd2ae50b31fb34d909b3944523abc
afshoang <kaitokid19xx@gmail.com>
2021-04-01 08:59:41 +0700

    delete product endpoint

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -23,4 +23,19 @@ const getProductById = asyncHandler(async (req, res) => {
   }
 });
 
-export { getProducts, getProductById };
+// @des Delete a product
+// @route DELETE /api/product/:id
+// @access Private/Admin
+const deleteProduct = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    await product.remove();
+    res.json({ message: 'Đã xóa sản phẩm!' });
+  } else {
+    res.status(404);
+    throw new Error('Product not found');
+  }
+});
+
+export { getProducts, getProductById, deleteProduct };
