65375dcfb05aca4da89a98cea580a0fed06a7f13
afshoang <kaitokid19xx@gmail.com>
2021-04-20 15:53:44 +0700

    Prepare for deloyment

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # MERN-mixmilk
+
 Đồ án tốt nghiệp sử dụng MERN stack
+
+## Các công nghệ sử dụng
+
+1. ReactJs
+2. Redux (State Management)
+3. NodeJs
+4. MongoDB
--- backend/server.js
+++ backend/server.js
@@ -24,10 +24,6 @@ if (process.env.NODE_ENV === 'development') {
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('API is running');
-});
-
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
@@ -36,6 +32,18 @@ app.use('/api/upload', uploadRoutes);
 
 const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  );
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running');
+  });
+}
 
 // Middleware for error handler
 app.use(notFound);
